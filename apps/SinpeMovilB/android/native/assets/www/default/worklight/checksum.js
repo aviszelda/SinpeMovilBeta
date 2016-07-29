7e481a80b5315440f12133beaaa533b6935baccf
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-07-29 11:23:10 -0600

    first beta
    first beta

---
+++ apps/SinpeMovilB/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1469811833003,"machine":"192.168.10.119","checksum":3492918047};
+/* Date: Fri Jul 29 11:03:53 CST 2016 */
---
+++ apps/SinpeMovilB/iphone/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1469811834422,"machine":"192.168.10.119","checksum":383010239};
+/* Date: Fri Jul 29 11:03:54 CST 2016 */
