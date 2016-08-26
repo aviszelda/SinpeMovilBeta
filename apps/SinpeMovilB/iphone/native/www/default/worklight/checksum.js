230c26a8cb502a9c79cc1f2bfcb5924a55176241
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-08-26 14:49:39 -0600

    working for iphone
    working for iphone

--- apps/SinpeMovilB/iphone/native/www/default/worklight/checksum.js
+++ apps/SinpeMovilB/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1472242811987,"machine":"192.168.10.119","checksum":2175924346};
-/* Date: Fri Aug 26 14:20:11 CST 2016 */
+var WL_CHECKSUM = {"date":1472243867006,"machine":"192.168.10.119","checksum":1252496265};
+/* Date: Fri Aug 26 14:37:47 CST 2016 */
