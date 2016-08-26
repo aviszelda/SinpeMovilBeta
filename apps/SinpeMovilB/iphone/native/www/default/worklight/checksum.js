8cd4f7ab2ca5941c1c459a0b64609d41d7761a06
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-08-26 14:30:05 -0600

    jquery library
    jquery library

--- apps/SinpeMovilB/iphone/native/www/default/worklight/checksum.js
+++ apps/SinpeMovilB/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1472232608945,"machine":"192.168.10.119","checksum":161683711};
-/* Date: Fri Aug 26 11:30:08 CST 2016 */
+var WL_CHECKSUM = {"date":1472242811987,"machine":"192.168.10.119","checksum":2175924346};
+/* Date: Fri Aug 26 14:20:11 CST 2016 */
